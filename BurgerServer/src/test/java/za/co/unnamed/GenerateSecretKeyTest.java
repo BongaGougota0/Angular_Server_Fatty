d5a5ba9e8117410e5fc403b77c5c7ec826749ba7
BongaGougota0 <bonga_gougota@outlook.com>
2025-02-18 13:47:59 +0200

    generation of secret key

---
+++ BurgerServer/src/test/java/za/co/unnamed/GenerateSecretKeyTest.java
@@ -0,0 +1,18 @@
+package za.co.unnamed;
+
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.junit.jupiter.api.Test;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
+
+public class GenerateSecretKeyTest {
+
+    @Test
+    public void generateSecretKey() {
+        SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+        String encodedKey = Base64.getEncoder().encodeToString(key.getEncoded());
+        System.out.println(String.format("Encoded key: %s", encodedKey));
+    }
+}
